4e40ec4d6a3db16ed829ae46e6ec4199b323ec02
OTI3NOST3VE <otienosteve711@icloud.com>
2026-06-03 00:13:27 +0300

    login screens

--- app/src/main/java/com/sc/hubmedia/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/sc/hubmedia/ui/screens/RegisterScreen.kt
@@ -1,14 +1,6 @@
 package com.sc.hubmedia.ui.screens
 
-import androidx.collection.emptyLongSet
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.Alignment
-import androidx.navigation.NavController
+import android.graphics.Outline
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -20,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -28,6 +21,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.School
 import androidx.compose.material.icons.filled.VideoLibrary
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -39,160 +33,188 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.sc.hubmedia.navigation.Screen
 import com.sc.hubmedia.ui.theme.MediaHubTheme
-
 @Composable
 fun RegisterScreen(navController: NavController){
     // data for the register screen i.e. state
-    var fullname by remember{mutableStateOf("")}
-    var email by remember {mutableStateOf("")}
-    var password by remember {mutableStateOf("")}
-    var confirmPassword by remember{mutableStateOf("")}
-    var passwordVisible by remember {mutableStateOf(false)}
-    var selectedRole by remember {mutableStateOf("Student")}
-    var roles = listOf("Student", "Teacher")
+    var fullname by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("")}
+    var passwordVisible by remember {
+        mutableStateOf(false) }
+    var selectedRole by remember {
+        mutableStateOf("Student") }
+    var roles = listOf("Student","Teacher")
     Box(modifier = Modifier.fillMaxSize().background(
         MaterialTheme.colorScheme.background
     )
-        ){
+    ){
         Column(modifier = Modifier.fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .padding(horizontal=24.dp, vertical = 48.dp),
-            horizontalAlignment = Alignment.CenterHorizontally) {
+            .padding(horizontal = 24.dp, vertical = 48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally){
             Icon(Icons.Default.VideoLibrary,
                 contentDescription = null,
                 tint=MaterialTheme.colorScheme.primary,
                 modifier = Modifier.size(64.dp))
-            Text(text ="Create Account",
-                style = MaterialTheme.typography.headlineMedium,
-                color=MaterialTheme.colorScheme.background)
+            Spacer(Modifier.height(8.dp))
+            Text(text="Create Account",
+                style= MaterialTheme.typography.headlineMedium,
+                color=MaterialTheme.colorScheme.onBackground)
             Spacer(Modifier.height(8.dp))
             Text(text="Join MediaHub Today",
-                style=MaterialTheme.typography.bodyLarge,
+                style= MaterialTheme.typography.bodyLarge,
                 color=MaterialTheme.colorScheme.onSurface.copy(
                     alpha = 0.5f
                 ))
             Spacer(Modifier.height(32.dp))
             // form inputs for registration
             OutlinedTextField(
-                Value = fullname,
-                onValueChange={fullname= it},
-                label ={Text("Fullname")},
-                leadingIcon={
-                    Icon(Icons.Default.Person,null)},
+                value = fullname,
+                onValueChange = {fullname = it},
+                label = {Text("Full Name")},
+                leadingIcon = {
+                    Icon(Icons.Default.Person
+                        ,null)},
                 singleLine = true,
                 modifier = Modifier.fillMaxWidth(),
-                shape =RoundedCornerShape(12.dp),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType=KeyboardType.Email
-                )
-
-
-
+                shape = RoundedCornerShape(12.dp)
             )
-            Spacer(modifier=Modifier.height(12.dp))
+            Spacer(modifier = Modifier.height(12.dp))
             OutlinedTextField(
-                Value = email,
-                onValueChange={email= it},
-                label ={Text("Fullname")},
-                leadingIcon={
-                    Icon(Icons.Default.Person,null)},
+                value = email,
+                onValueChange = {email = it},
+                label = {Text("Email")},
+                leadingIcon = {
+                    Icon(Icons.Default.Email
+                        ,null)},
                 singleLine = true,
                 modifier = Modifier.fillMaxWidth(),
-                shape =RoundedCornerShape(12.dp),
+                shape = RoundedCornerShape(12.dp),
                 keyboardOptions = KeyboardOptions(
-                    keyboardType=KeyboardType.Email
+                    keyboardType = KeyboardType.Email
                 )
             )
-            Spacer(modifier=Modifier.height(12.dp))
-            //  confirm password
+            Spacer(Modifier.height(12.dp))
+            //password
             OutlinedTextField(
-                value = confirmPassword,
+                value = password,
+                onValueChange = {password = it},
+                label = {Text("Password")},
+                leadingIcon = {
+                    Icon(Icons.Default.Lock
+                        ,null)},
+                trailingIcon = {
+                    IconButton(onClick = {
+                        passwordVisible = !passwordVisible
+                    }) {
+                        Icon(
+                            if(passwordVisible)
+                                Icons.Default.VisibilityOff else
+                                Icons.Default.Visibility,
+                            null
+                        )
+                    }
+                },
+                visualTransformation = if(passwordVisible)
+                    VisualTransformation.None else
+                    PasswordVisualTransformation(),
+                singleLine = true,
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(Modifier.height(12.dp))
+            // confirm password
+            OutlinedTextField(
+                value=confirmPassword,
                 onValueChange = {confirmPassword = it},
                 leadingIcon = {
-                    Icon(Icons.Default.Lock,null)
-                },
+                    Icon(Icons.Default.Lock,
+                        null)},
                 visualTransformation = PasswordVisualTransformation(),
                 singleLine = true,
                 shape = RoundedCornerShape(12.dp),
                 modifier = Modifier.fillMaxWidth()
             )
-            Spacer (Modifier.height(20.dp))
+            Spacer(Modifier.height(20.dp))
             // role selector : dropdown
-            Text("select Role",
-                style= MaterialTheme.typography.labelSmall,
+            Text("Select Role",
+                style=MaterialTheme.typography.labelSmall,
                 modifier = Modifier.align(Alignment.Start),
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f))
+                color= MaterialTheme.colorScheme.onSurface
+                    .copy(alpha = 0.7f))
             Spacer(Modifier.height(8.dp))
             Row(modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(12.dp)
+                horizontalArrangement = Arrangement.spacedBy(
+                    12.dp
+                )
             ) {
                 // present options to the user
                 roles.forEach { role ->
                     // capture what role user selects
                     val selected = selectedRole == role
-                    // composable to present the options
+                    //composable to present the options
                     FilterChip(
                         selected = selected,
                         onClick = {selectedRole = role},
-                        modifier = Modifier.weight(1f),
+                        label = {Text(role)},
+                        modifier= Modifier.weight(1f),
                         leadingIcon = {
-                            Icon(if role == "Teacher") Icons.Default.School else Icons.Default.Person, null
-                            modifier = Modifier.size()
+                            Icon(
+                                if(role == "Teacher") Icons.Default.School
+                                else
+                                    Icons.Default.Person,null,
+                                modifier = Modifier.size(18.dp)
+                            )
                         }
                     )
-
                 }
             }
             Spacer(Modifier.height(28.dp))
-            // register button
+            // Register Button
             Button(
-                onClick = {},
-                modifier = Modifier.fillMaxWidth().height(52.dp),
+                onClick={},
+                modifier=Modifier.fillMaxWidth().height(52.dp),
                 shape = RoundedCornerShape(12.dp)
-
-            ){
-                Text(text = "Create Account",
-                    style = MaterialTheme.typography.bodyLarge)
-
-            }
+            ){  Text("Create Account",
+                style = MaterialTheme.typography.bodyLarge)}
             Spacer(Modifier.height(16.dp))
-            // To link to the register string
+            // to link to the register screen
             TextButton(
                 onClick = {
                     navController.navigate(
-                        Screen.Login.route
-                    ){
-                        popUpTo(Screen.Register.route){inclusive = true}
+                        Screen.Login.route){
+                        popUpTo(Screen.Register.route) {inclusive = true}
                     }
                 },
                 modifier = Modifier.align(Alignment.End)
             ) {
                 Text("Already have an account? Sign In",
-                    color= MaterialTheme.colorScheme.primary)
+                    color=MaterialTheme.colorScheme.primary)
             }
-
-
-
         }
     }
-
 }
+
 @Preview(showBackground = true)
 @Composable
 fun RegisterScreenPreview(){
     MediaHubTheme {
-        RegisterScreen(rememberNavController())
-    }
+        RegisterScreen(rememberNavController())}
 }
