dd15d3e5cfecadce98005580905668de4b8c83e9
otienosteve711 <otienosteve711@gmail.com>
2026-06-09 16:26:06 +0300

    logout

--- app/src/main/java/com/sc/hubmedia/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/sc/hubmedia/ui/screens/DashboardScreen.kt
@@ -1,8 +1,35 @@
 package com.sc.hubmedia.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-@Composable
-fun DashboardScreen(navController: NavController){
+import com.sc.hubmedia.navigation.Screen
+import com.sc.hubmedia.viewmodel.AuthViewModel
 
-}
+@Composable
+fun DashboardScreen(navController: NavController, authViewModel: AuthViewModel= viewModel()){
+    val authState by authViewModel.authState.collectAsState()
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+
+            Button(onClick = {
+                authViewModel.logout()
+                navController.navigate(Screen.Login.route) {
+                    popUpTo(Screen.Dashboard.route) { inclusive = true }
+                }
+
+            }) {
+                Text("Logout")
+            }
+        }
+    }
+
