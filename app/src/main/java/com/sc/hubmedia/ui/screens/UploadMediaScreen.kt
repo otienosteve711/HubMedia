cb74b8f3bbd0aa640f00d7f0ddd389aa97c9e2d5
otienosteve711 <otienosteve711@gmail.com>
2026-06-19 16:33:41 +0300

    uploads

--- app/src/main/java/com/sc/hubmedia/ui/screens/UploadMediaScreen.kt
+++ app/src/main/java/com/sc/hubmedia/ui/screens/UploadMediaScreen.kt
@@ -1,8 +1,172 @@
 package com.sc.hubmedia.ui.screens
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.Upload
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.sc.hubmedia.viewmodel.AuthViewModel
+import com.sc.hubmedia.viewmodel.MediaState
+import com.sc.hubmedia.viewmodel.MediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UploadMediaScreen(navController: NavController){
+fun UploadMediaScreen(navController: NavController, authViewModel: AuthViewModel= viewModel(),
+                      mediaViewModel: MediaViewModel=viewModel()){
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+    var category by remember { mutableStateOf("Video") }
+    var isPublic by remember { mutableStateOf(true) }
+    var mediaUri by remember { mutableStateOf<Uri?>(null) }
+    val categories = listOf("Video","Document", "Image", "Audio")
+
+    // Media Picker Launcher : this allows us to collect items from devices
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? -> mediaUri = uri} // storing the path
+    // references to our viewmodel states
+    val mediaState by mediaViewModel.mediaState.collectAsState()
+    val uploadProgress by mediaViewModel.uploadProgress.collectAsState()
+    val profile by authViewModel.currentProfile.collectAsState()
+    val isLoading = mediaState is MediaState.Loading
+    val errorMessage = (mediaState as? MediaState.Error)?.message
+    // Launched effect i.e. first execution when user is on screen
+    // clear any state
+    LaunchedEffect(mediaState) {
+        if (mediaState is MediaState.Success){
+            mediaViewModel.clearState()
+            navController.popBackStack()
+        }
+    }
+    // user interface for the screen
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Upload Media") },
+                navigationIcon = {
+                    IconButton(onClick = {navController.popBackStack()}){
+                        Icon(Icons.Default.ArrowBack,"Back")
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface
+                )
+            )
+        },
+        containerColor = MaterialTheme.colorScheme.surface
+    ) { padding ->
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(padding)
+                .verticalScroll(rememberScrollState())
+                .padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            // error messages display
+            errorMessage?.let {
+                Card(
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.error),
+                    shape = RoundedCornerShape(12.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.padding(12.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(Icons.Default.Error,null,
+                            tint = MaterialTheme.colorScheme.onError )
+                        Spacer(Modifier.width(8.dp))
+                        Text(it, color = MaterialTheme.colorScheme.onError)
+                    }
+                }
+            }
+        }
+        // Media Picker
+        Box(
+            modifier = Modifier.fillMaxWidth().height(200.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.surface)
+                .border(
+                    2.dp,if (mediaUri !=null) MaterialTheme.colorScheme.primary else
+                        MaterialTheme.colorScheme.onSurface.copy(0.2f),
+                    RoundedCornerShape(16.dp)
+                ).clickable{ launcher.launch("*/*")},
+            contentAlignment = Alignment.Center
+        ) {
+            // show user path of selected media
+            if (mediaUri != null){
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                ) {Icon(Icons.Default.CheckCircle,
+                    null, modifier = Modifier.size(32.dp),
+                    tint = MaterialTheme.colorScheme.primary)
+                Spacer(Modifier.height(8.dp))
+                Text(mediaUri.toString(),
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    maxLines = 2, overflow = TextOverflow.Ellipsis,
+                    textAlign = TextAlign.Center)
+                }
+
+            } else {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                ) {Icon(Icons.Default.Upload,
+                    null, modifier = Modifier.size(32.dp),
+                    tint = MaterialTheme.colorScheme.primary)
+                    Spacer(Modifier.height(8.dp))
+                    Text("Tap to Select File",
+                        style = MaterialTheme.typography.labelSmall,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        textAlign = TextAlign.Center)
+                }
+
+
+            }
+        }
+    }
+
 
 }
