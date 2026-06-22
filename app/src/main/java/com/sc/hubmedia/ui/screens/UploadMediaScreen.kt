1bb8730a0b3c6a6b9b758eb867496639c8980102
otienosteve711 <otienosteve711@gmail.com>
2026-06-22 16:27:53 +0300

    upload data

--- app/src/main/java/com/sc/hubmedia/ui/screens/UploadMediaScreen.kt
+++ app/src/main/java/com/sc/hubmedia/ui/screens/UploadMediaScreen.kt
@@ -23,18 +23,26 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.CloudUpload
 import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Public
 import androidx.compose.material.icons.filled.Title
 import androidx.compose.material.icons.filled.Upload
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -44,6 +52,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -56,18 +66,19 @@ import com.sc.hubmedia.viewmodel.MediaViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UploadMediaScreen(navController: NavController, authViewModel: AuthViewModel= viewModel(),
-                      mediaViewModel: MediaViewModel=viewModel()){
+                      mediaViewModel: MediaViewModel=viewModel()) {
+    val context = LocalContext.current // tags current screen
     var title by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
     var category by remember { mutableStateOf("Video") }
     var isPublic by remember { mutableStateOf(true) }
     var mediaUri by remember { mutableStateOf<Uri?>(null) }
-    val categories = listOf("Video","Document", "Image", "Audio")
+    val categories = listOf("Video", "Document", "Image", "Audio")
 
     // Media Picker Launcher : this allows us to collect items from devices
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.GetContent()
-    ) { uri: Uri? -> mediaUri = uri} // storing the path
+    ) { uri: Uri? -> mediaUri = uri } // storing the path
     // references to our viewmodel states
     val mediaState by mediaViewModel.mediaState.collectAsState()
     val uploadProgress by mediaViewModel.uploadProgress.collectAsState()
@@ -77,7 +88,7 @@ fun UploadMediaScreen(navController: NavController, authViewModel: AuthViewModel
     // Launched effect i.e. first execution when user is on screen
     // clear any state
     LaunchedEffect(mediaState) {
-        if (mediaState is MediaState.Success){
+        if (mediaState is MediaState.Success) {
             mediaViewModel.clearState()
             navController.popBackStack()
         }
@@ -88,8 +99,8 @@ fun UploadMediaScreen(navController: NavController, authViewModel: AuthViewModel
             TopAppBar(
                 title = { Text("Upload Media") },
                 navigationIcon = {
-                    IconButton(onClick = {navController.popBackStack()}){
-                        Icon(Icons.Default.ArrowBack,"Back")
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Default.ArrowBack, "Back")
                     }
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
@@ -116,76 +127,183 @@ fun UploadMediaScreen(navController: NavController, authViewModel: AuthViewModel
                         modifier = Modifier.padding(12.dp),
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Icon(Icons.Default.Error,null,
-                            tint = MaterialTheme.colorScheme.onError )
+                        Icon(
+                            Icons.Default.Error, null,
+                            tint = MaterialTheme.colorScheme.onError
+                        )
                         Spacer(Modifier.width(8.dp))
                         Text(it, color = MaterialTheme.colorScheme.onError)
                     }
                 }
             }
-        }
-        // Media Picker
-        Box(
-            modifier = Modifier.fillMaxWidth().height(200.dp)
-                .clip(RoundedCornerShape(16.dp))
-                .background(MaterialTheme.colorScheme.surface)
-                .border(
-                    2.dp,if (mediaUri !=null) MaterialTheme.colorScheme.primary else
-                        MaterialTheme.colorScheme.onSurface.copy(0.2f),
-                    RoundedCornerShape(16.dp)
-                ).clickable{ launcher.launch("*/*")},
-            contentAlignment = Alignment.Center
-        ) {
-            // show user path of selected media
-            if (mediaUri != null){
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                ) {Icon(Icons.Default.CheckCircle,
-                    null, modifier = Modifier.size(32.dp),
-                    tint = MaterialTheme.colorScheme.primary)
-                Spacer(Modifier.height(8.dp))
-                Text(mediaUri.toString(),
-                    style = MaterialTheme.typography.labelSmall,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    maxLines = 2, overflow = TextOverflow.Ellipsis,
-                    textAlign = TextAlign.Center)
-                }
 
-            } else {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                ) {Icon(Icons.Default.Upload,
-                    null, modifier = Modifier.size(32.dp),
-                    tint = MaterialTheme.colorScheme.primary)
-                    Spacer(Modifier.height(8.dp))
-                    Text("Tap to Select File",
+            // Media Picker
+            Box(
+                modifier = Modifier.fillMaxWidth().height(200.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(MaterialTheme.colorScheme.surface)
+                    .border(
+                        2.dp, if (mediaUri != null) MaterialTheme.colorScheme.primary else
+                            MaterialTheme.colorScheme.onSurface.copy(0.2f),
+                        RoundedCornerShape(16.dp)
+                    ).clickable { launcher.launch("*/*") },
+                contentAlignment = Alignment.Center
+            ) {
+                // show user path of selected media
+                if (mediaUri != null) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    ) {
+                        Icon(
+                            Icons.Default.CheckCircle,
+                            null, modifier = Modifier.size(32.dp),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                        Spacer(Modifier.height(8.dp))
+                        Text(
+                            mediaUri.toString(),
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            maxLines = 2, overflow = TextOverflow.Ellipsis,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+
+                } else {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(horizontal = 16.dp)
+                    ) {
+                        Icon(
+                            Icons.Default.Upload,
+                            null, modifier = Modifier.size(32.dp),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                        Spacer(Modifier.height(8.dp))
+                        Text(
+                            "Tap to Select File",
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+
+
+                }
+            }
+            // inputs for title and description collections
+            OutlinedTextField(
+                value = title, onValueChange = { title = it },
+                label = { Text("Title") },
+                leadingIcon = { Icon(Icons.Default.Title, null) },
+                minLines = 3, modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp)
+            )
+            OutlinedTextField(
+                value = description, onValueChange = { description = it },
+                label = { Text("Description") },
+                leadingIcon = { Icon(Icons.Default.Description, null) },
+                minLines = 3, modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp)
+            )
+            // TODO : add category picker, public toggle, btn
+            Text(
+                "Category", style = MaterialTheme.typography.labelSmall,
+                color = Color.Blue
+            )
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                categories.forEach { cat ->
+                    FilterChip(
+                        selected = category == cat,
+                        onClick = { category = cat },
+                        label = { Text(cat) }
+                    )
+                }
+            }
+            // visibility toggle - switch on for isPublic off for private
+            Card(
+                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(
+                        horizontal = 16.dp,
+                        vertical = 12.dp
+                    ), verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        if (isPublic) Icons.Default.Public else Icons.Default.Lock, null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(Modifier.width(12.dp))
+                    Column(Modifier.weight(1f)) {
+                        Text(
+                            if (isPublic) "Public" else "Private",
+                            style = MaterialTheme.typography.bodyLarge,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                        Text(
+                            if (isPublic) "Visible to All users" else "Only Visible to You",
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
+                    // Switch : toggle interface
+                    Switch(
+                        checked = isPublic, onCheckedChange = { isPublic = it }
+                    )
+                }
+            }
+            // progress bar = show user percentage of upload
+            if (isLoading && uploadProgress > 0f) {
+                Column {
+                    LinearProgressIndicator(
+                        progress = uploadProgress,
+                        modifier = Modifier.fillMaxWidth(),
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(Modifier.height(4.dp))
+                    Text(
+                        "Uploading.... ${(uploadProgress * 100).toInt()}%",
                         style = MaterialTheme.typography.labelSmall,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        textAlign = TextAlign.Center)
+                        color = Color.Blue
+                    )
                 }
-
 
             }
+            // button to submit data to firestore
+            Button(
+                onClick = {
+                    mediaUri?.let { uri ->
+                        mediaViewModel.uploadMedia(
+                            context, title, description, category, isPublic,
+                            ownerName = profile?.fullname ?: "Anonymous", uri
+
+                        )
+
+                    }
+                },
+                enabled = title.isNotBlank() && description.isNotBlank() && mediaUri != null && !isLoading,
+                modifier = Modifier.fillMaxWidth().height(52.dp),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                if (isLoading) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(20.dp),
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        strokeWidth = 2.dp
+                    )
+                } else {
+                    Icon(Icons.Default.CloudUpload, null)
+                    Spacer(Modifier.width(8.dp))
+                    Text("Upload Media")
+                }
+            }
+
+
         }
-        // inputs for title and description collections
-        OutlinedTextField(
-            value = title, onValueChange = {title = it},
-            label = {Text("Title")},
-            leadingIcon = {Icon(Icons.Default.Title,null)},
-            minLines = 3, modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(12.dp)
-        )
-        OutlinedTextField(
-            value = description, onValueChange = {description = it},
-            label = {Text("Description")},
-            leadingIcon = {Icon(Icons.Default.Description,null)},
-            minLines = 3, modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(12.dp)
-        )
-        // TODO : add category picker, public toggle, btn
+
+
     }
-
-
 }
